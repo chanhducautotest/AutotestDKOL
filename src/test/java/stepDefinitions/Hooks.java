ac7dbd85749d5cc20c8c9826d6bdfa8530d05a72
minhlh5 <minhlh5@fpt.com.vn>
2023-03-27 17:31:59 +0700

    Fix lỗi NoSuchSessionException khi chạy trên grid

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -4,8 +4,12 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import net.fpt.driver_setting.DriverBase;
+import org.openqa.selenium.NoSuchSessionException;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import runners.RunCucumberByCompositionTest;
 
 import java.io.IOException;
 
@@ -13,17 +17,22 @@ import static net.fpt.driver_setting.DriverBase.closeDriverObjects;
 import static net.fpt.driver_setting.DriverBase.instantiateDriverObject;
 
 public class Hooks {
-
+    static final Logger logger = LoggerFactory.getLogger(Hooks.class);
     @Before
     public void setUp() throws Exception {
         instantiateDriverObject();
     }
 
     @After
-    public void embedScreenshot(Scenario scenario) throws IOException, InterruptedException {
+    public void embedScreenshot(Scenario scenario) throws IOException, InterruptedException, NoSuchSessionException {
         byte[] screenshot = ((TakesScreenshot) DriverBase.getDriver()).getScreenshotAs(OutputType.BYTES);
         scenario.attach(screenshot, "image/png", "name");
         Thread.sleep(3000);
-        closeDriverObjects();
+        try {
+            closeDriverObjects();
+        }catch (org.openqa.selenium.NoSuchSessionException ex){
+            logger.info("[NoSuchSessionException TestNGListenerP onTestSuccess]"+ex);
+        }
+
     }
 }
